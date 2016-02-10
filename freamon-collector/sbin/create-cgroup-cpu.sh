be9b4c46e6e8be4249ce982287a6640c43ff22d4
Gjum <code.gjum@gmail.com>
2016-02-10 17:31:56 +0100

    Add setup scripts and example configs for yarn with cgroups

---
+++ freamon-collector/sbin/create-cgroup-cpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# user group that the YARN users will run under
+# same as yarn.nodemanager.linux-container-executor.group
+CONTAINERGROUP=ldapusers
+
+# cgroup hierarchy under which to place YARN proccesses
+# can be any path, not just single dir
+# same as yarn.nodemanager.linux-container-executor.cgroups.hierarchy
+CGROUPSHIERARCHY=hadoop-yarn
+
+# same as yarn.nodemanager.linux-container-executor.cgroups.mount-path
+MOUNTPATH=/sys/fs/cgroup
+
+GROUPPATH=${MOUNTPATH}/cpu,cpuacct/${CGROUPSHIERARCHY}
+mkdir ${GROUPPATH}
+chown :${CONTAINERGROUP} ${GROUPPATH}
+chmod g+w ${GROUPPATH}
