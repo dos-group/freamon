3f24a805641bdbf8a8ed3d0fc131d40711b9df42
Gjum <code.gjum@gmail.com>
2016-02-15 19:16:19 +0100

    Start script appends to logs instead of replacing them each time

--- start-cluster.sh
+++ start-cluster.sh
@@ -5,7 +5,7 @@
 #         (see freamon-monitor/src/main/resources/hosts), e.g. ./start-cluster wally
 
 # preparation:
-# 1. provide a slaves file, e.g. by soft-linking Hadoop's slave file or 
+# 1. provide a slaves file, e.g. by soft-linking Hadoop's slave file or
 #    one from freamon-monitor/src/main/resources/hosts/
 # 2. build system: mvn clean package
 
@@ -13,7 +13,6 @@
 # - execute this script on the node that runs the master
 # - the script assumes passwordless ssh-access to all slaves and this project dir on all workers
 
-# TODO: preserve older logs by appending a number like Hadoop and Flink do it
 # TODO: better argument parsing, add -h flag for the host argument
 
 HOST_CONFIG=$1
@@ -38,13 +37,13 @@ fi
 
 if [ ! -f "$SLAVES_FILE" ]
 then
-    echo "No slaves file in $PWD"
+    echo "No slaves file $SLAVES_FILE"
     exit 1
 fi
 
 if [ -f "$MASTER_PID_FILE" ]
 then
-    echo "System appears to be running, run ./stop-cluster.sh before"
+    echo "System appears to be running, stop it first with ./stop-cluster.sh"
     exit 1
 fi
 
@@ -53,20 +52,20 @@ mkdir -p $LOG_FOLDER
 echo "Using $JAVA_BIN"
 
 echo "Starting freamon master system"
-java -cp $ABSOLUTE_JAR_PATH $MASTER_CLASS -h $HOST_CONFIG >$MASTER_LOG_FILE 2>$MASTER_ERR_FILE & echo $! >$MASTER_PID_FILE
+$JAVA_BIN -cp $ABSOLUTE_JAR_PATH $MASTER_CLASS -h $HOST_CONFIG >>$MASTER_LOG_FILE 2>>$MASTER_ERR_FILE & echo $! >$MASTER_PID_FILE
 echo "Started freamon master on $HOSTNAME (PID=$(cat $MASTER_PID_FILE))"
 
 for SLAVE in $(cat $SLAVES_FILE ) ; do
-    
+
     echo "Starting freamon worker system on $SLAVE"
 
     WORKER_LOG_FILE="$LOG_FOLDER/$SLAVE-worker.out"
     WORKER_ERR_FILE="$LOG_FOLDER/$SLAVE-worker.err"
     WORKER_PID_FILE="$LOG_FOLDER/$SLAVE-worker.pid"
-   
+
     CMD="$JAVA_BIN -cp $ABSOLUTE_JAR_PATH $WORKER_CLASS -h $HOST_CONFIG"
-    ssh "$SLAVE" "nohup $CMD >$WORKER_LOG_FILE 2>$WORKER_ERR_FILE & echo \$!" > $WORKER_PID_FILE
-   
+    ssh "$SLAVE" "nohup $CMD >>$WORKER_LOG_FILE 2>>$WORKER_ERR_FILE & echo \$!" >$WORKER_PID_FILE
+
     echo "Started freamon worker system on $SLAVE (PID=$(cat $WORKER_PID_FILE))"
-    
+
 done
