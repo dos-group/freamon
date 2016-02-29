e4cd070350468e289cb0d69f0a72639fd0b6b444
Thomas Renner <thomas.renner@tu-berlin.de>
2016-02-29 19:08:04 +0100

    Fix hadoop in secure mode permissions for cluster setup

--- freamon-collector/sbin/setup-permissions.sh
+++ freamon-collector/sbin/setup-permissions.sh
@@ -4,12 +4,13 @@ echo 'Please update the installpath variable!'
 exit 1
 
 installpath=/path/to/hadoop-VERSION/
+chmod 755 $installpath/bin/
 
 chown root $installpath/bin/container-executor
 chmod 6050 $installpath/bin/container-executor
 
 chown root $installpath/etc/hadoop/container-executor.cfg
-chmod  400 $installpath/etc/hadoop/container-executor.cfg
+chmod  644 $installpath/etc/hadoop/container-executor.cfg
 
 # every directory from $installpath/etc/hadoop up to / has to be owned by root
 currentpath=/
@@ -18,3 +19,7 @@ do currentpath=$currentpath$dir/
    chown root:ldapusers $currentpath
    chmod 755 $currentpath
 done
+
+# YARN cannot create log folder when directory is owned by root
+mkdir $installpath/logs
+chown bbdc:ldapusers $installpath/logs
