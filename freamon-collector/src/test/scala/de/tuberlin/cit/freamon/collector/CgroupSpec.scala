41805c01a5ee8ecf451c664478491740daeaa536
Sascha Wolke <dersascha@users.noreply.github.com>
2016-04-24 18:32:52 +0200

    scala test+mock and some collector tests added

---
+++ freamon-collector/src/test/scala/de/tuberlin/cit/freamon/collector/CgroupSpec.scala
@@ -0,0 +1,40 @@
+package de.tuberlin.cit.freamon.collector
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.FlatSpec
+
+class CgroupSpec extends FlatSpec with MockFactory {
+  val mountPath = getClass.getResource("/sys-fs-cgroup").getPath
+  val groupId = "hadoop-yarn/container_1455551433868_0002_01_123456"
+
+  def withCgroup(testCode: (Cgroup) => Any) {
+    val cgroup = new Cgroup(mountPath, groupId)
+    testCode(cgroup)
+  }
+
+  "getCurrentBlockIOUsage" should "sum up all sectors" in withCgroup { cgroup =>
+    assert(cgroup.getCurrentBlockIOUsage == (2856 + 13224))
+  }
+
+  "parseNetworkUsage" should "sum up all rx and tx values" in withCgroup { cgroup =>
+    val taskNetStatFile = getClass.getResource("/proc/1234/net/dev").getPath
+    assert(cgroup.parseNetworkUsage(taskNetStatFile) == (15651 + 648))
+  }
+
+  "parseNetworkUsage" should "return 0 if file is missing (task finished)" in withCgroup { cgroup =>
+    assert(cgroup.parseNetworkUsage("not-existing-path") == 0l)
+  }
+
+  "getCurrentNetworkUsage" should "return network usage of all container tasks" in {
+    val parseMock = mockFunction[String, Long]
+    parseMock.expects("/proc/12/net/dev").returns(10).twice // init + call = twice
+    parseMock.expects("/proc/34/net/dev").returns(20).twice
+    parseMock.expects("/proc/56/net/dev").returns(30).twice
+
+    val cgroupMock = new Cgroup(mountPath, groupId) {
+      override def parseNetworkUsage(path: String): Long = parseMock(path)
+    }
+
+    assert(cgroupMock.getCurrentNetworkUsage == 60)
+  }
+}
