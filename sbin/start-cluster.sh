8ed90b7ec8c84d64de48ca11fb0b9ac22f6829fa
Gjum <code.gjum@gmail.com>
2016-06-14 21:01:14 +0200

    update {start,stop}-cluster.sh

--- sbin/start-cluster.sh
+++ sbin/start-cluster.sh
@@ -6,6 +6,7 @@
 
 # Preparation:
 # - build system: mvn clean package
+# - extract hyperic-sigar-1.6.4.tar.gz to /your/freamon/installation/lib/hyperic-sigar-1.6.4/
 
 # Assumptions:
 # - execute this script on the node that runs the master
@@ -21,6 +22,7 @@ CLUSTER_CONFIG="$(readlink -f "$1")"
 
 SLAVES_FILE="$HADOOP_PREFIX/etc/hadoop/slaves"
 ABSOLUTE_JAR_PATH="$(readlink -f freamon-monitor/target/freamon-monitor-1.0-SNAPSHOT-allinone.jar)"
+LD_LIBRARY_PATH="$(readlink -f "lib/hyperic-sigar-1.6.4/sigar-bin/lib/")"
 LOG_FOLDER="$(readlink -f "logs")"
 MASTER_CLASS="de.tuberlin.cit.freamon.monitor.actors.MonitorMasterSystem"
 MASTER_LOG_FILE="$LOG_FOLDER/$HOSTNAME-master.out"
@@ -76,7 +78,7 @@ for SLAVE in $(cat $SLAVES_FILE ) ; do
     WORKER_ERR_FILE="$LOG_FOLDER/$SLAVE-worker.err"
     WORKER_PID_FILE="$LOG_FOLDER/$SLAVE-worker.pid"
 
-    CMD="$JAVA_BIN -cp '$ABSOLUTE_JAR_PATH' '$WORKER_CLASS' -c '$CLUSTER_CONFIG'"
+    CMD="$JAVA_BIN -Djava.library.path='$LD_LIBRARY_PATH' -cp '$ABSOLUTE_JAR_PATH' '$WORKER_CLASS' -c '$CLUSTER_CONFIG'"
     ssh "$SLAVE" "nohup $CMD >>$WORKER_LOG_FILE 2>>$WORKER_ERR_FILE & echo \$!" >$WORKER_PID_FILE
 
     echo "Started freamon worker system on $SLAVE (PID=$(cat $WORKER_PID_FILE))"
