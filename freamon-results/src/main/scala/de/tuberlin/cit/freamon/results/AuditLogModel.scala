98f79752822ea6cdd660343b49b5fa50b8cb8818
Adrian Warszewski <a.warszewski@campus.tu-berlin.de>
2016-09-06 17:50:52 +0200

    AuditLogModel Specification for MonetDB storage of HDFS Audit Logs.

---
+++ freamon-results/src/main/scala/de/tuberlin/cit/freamon/results/AuditLogModel.scala
@@ -0,0 +1,126 @@
+package de.tuberlin.cit.freamon.results
+
+/**
+  * Model class for HDFS Audit logs collected by the master actor from agent actors
+  */
+case class AuditLogModel(
+                          date: Long,
+                          allowed: Boolean,
+                          ugi: String,
+                          ip: String,
+                          cmd: String,
+                          src: String,
+                          dest: String,
+                          perm: String,
+                          proto: String
+                        )
+
+object AuditLogModel extends PersistedAPI[AuditLogModel]{
+
+  import java.sql.Connection
+  import anorm.SqlParser._
+  import anorm._
+
+  override val tableName: String = "audit_log_entry"
+
+  override val rowParser = {
+    get[Long]     ("date")    ~
+      get[Boolean]  ("allowed") ~
+      get[String]   ("ugi")     ~
+      get[String]   ("ip")      ~
+      get[String]   ("cmd")     ~
+      get[String]   ("src")     ~
+      get[String]   ("dest")    ~
+      get[String]   ("perm")    ~
+      get[String]   ("proto")     map{
+      case date ~ allowed ~ ugi ~ ip ~ cmd ~ src ~ dest ~ perm ~ proto => AuditLogModel(
+        date,
+        allowed,
+        ugi,
+        ip,
+        cmd,
+        src,
+        dest,
+        perm,
+        proto)
+    }
+  }
+
+  override def createTable()(implicit conn: Connection): Unit = if(!tableExists) {
+    SQL(
+      s"""
+       CREATE TABLE $tableName (
+        date    BIGINT    NOT NULL,
+        allowed BOOLEAN   NOT NULL,
+        ugi     TEXT      NOT NULL,
+        ip      TEXT      NOT NULL,
+        cmd     TEXT      NOT NULL,
+        src     TEXT              ,
+        dest    TEXT              ,
+        perm    TEXT              ,
+        proto   TEXT
+     """).execute()
+  }
+
+  private val fields = "date, allowed, ugi, ip, cmd, src, dest, perm, proto"
+
+  override def insert(x: AuditLogModel)(implicit conn: Connection): Unit = {
+    SQL(
+      s"""
+         INSERT INTO $tableName($fields) VALUES(
+         '${x.date}',
+         '${x.allowed}',
+         '${x.ugi},
+         '${x.ip}',
+         '${x.cmd}',
+         '${x.src},
+         '${x.dest}',
+         '${x.perm}',
+         '${x.proto}'
+         )
+       """).executeInsert()
+  }
+
+  override def insert(xs: Seq[AuditLogModel])(implicit conn: Connection): Unit = if (xs.nonEmpty) singleCommit{
+    BatchSql(
+      s"""
+         INSERT INTO $tableName($fields) VALUES(
+         '{date}',
+         '{allowed}',
+         '{ugi}',
+         '{ip}',
+         '{cmd}',
+         '{src}',
+         '{dest}',
+         '{perm}',
+         '{proto}'
+         )
+       """,
+      namedParametrsFor(xs.head),
+      xs.tail.map(namedParametrsFor): _*
+    ).execute()
+  }
+
+  override def update(x: AuditLogModel)(implicit conn: Connection): Unit = {
+    throw new NotImplementedError("AuditLogModel objects are immutable, updates are not supported")
+  }
+
+  override def delete(x: AuditLogModel)(implicit conn: Connection): Unit = {
+    throw new NotImplementedError("AuditLogModel objects cannot be deleted")
+  }
+
+
+  def namedParametrsFor(x: AuditLogModel): Seq[NamedParameter] = Seq[NamedParameter](
+    'date -> x.date,
+    'allowed -> x.allowed,
+    'ugi -> x.ugi,
+    'ip -> x.ip,
+    'cmd -> x.cmd,
+    'src -> x.src,
+    'dest -> x.dest,
+    'perm -> x.perm,
+    'proto -> x.proto
+  )
+
+
+}
