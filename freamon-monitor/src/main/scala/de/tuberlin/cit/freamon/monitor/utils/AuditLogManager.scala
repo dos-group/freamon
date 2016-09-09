eac26c8c00688ec548ed52f1f1efad21904d2b80
Adrian Warszewski <a.warszewski@campus.tu-berlin.de>
2016-09-09 16:24:24 +0200

    Move methods responsible for reading the file into AuditLogManager

--- freamon-monitor/src/main/scala/de/tuberlin/cit/freamon/monitor/utils/AuditLogManager.scala
+++ freamon-monitor/src/main/scala/de/tuberlin/cit/freamon/monitor/utils/AuditLogManager.scala
@@ -1,8 +1,9 @@
 package de.tuberlin.cit.freamon.monitor.utils
 
-import de.tuberlin.cit.freamon.collector.AuditLogCollector
-import de.tuberlin.cit.freamon.results.AuditLogModel
+import java.io.{File, InputStream}
 import java.sql.{Connection, DriverManager}
+
+import de.tuberlin.cit.freamon.results.AuditLogModel
 
 /**
   * Object handling the communication between the log reader and database writer
@@ -16,7 +17,7 @@ object AuditLogManager {
       pathToLog = args(3)
       loadDriver("nl.cwi.monetdb.jdbc.MonetDriver")
       createTable()
-      AuditLogCollector.start(args(3))
+      start(args(3))
     }
   }
 
@@ -34,6 +35,52 @@ object AuditLogManager {
   def createTable(): Unit = {
     implicit val conn = getConnection("jdbc:monetdb://localhost/freamon", "monetdb", "monetdb")
     AuditLogModel.createTable()
+  }
+  def start(args: String): Unit = {
+    import java.io.{BufferedReader, File, InputStreamReader}
+
+    val logFile = new File(args)
+    val br = new BufferedReader(new InputStreamReader(follow(logFile)))
+
+    def read: Unit = {
+      val l = br.readLine
+      if(l != null){
+        println(l)
+        read
+      }
+    }
+    read
+  }
+  def follow(file: File): InputStream = {
+    val maxRetries = 3
+    val waitToOpen = 1000
+    val waitBetweenReads = 100
+
+    def sleep(msec: Long) = () => Thread.sleep(msec)
+
+    follow(file, maxRetries, sleep(waitToOpen), sleep(waitBetweenReads))
+  }
+
+  def follow(file: File, openTries: Int, openSleep: () => Unit, rereadSleep: () => Unit): InputStream = {
+    import java.io.SequenceInputStream
+
+    val e = new java.util.Enumeration[InputStream]() {
+      def nextElement = new FollowingInputStream(file, rereadSleep)
+      def hasMoreElements = testExists(file, openTries, openSleep)
+    }
+
+    new SequenceInputStream(e)
+  }
+
+  def testExists(file: File, tries: Int, sleep: () => Unit): Boolean = {
+    def tryExists(n: Int): Boolean =
+      if (file.exists) true
+      else if (n > tries) false
+      else {
+        sleep()
+        tryExists(n+1)
+      }
+    tryExists(1)
   }
 
 }
