b07be6612d96129a0f68bd8d406389a14adc588f
Adrian Warszewski <a.warszewski@campus.tu-berlin.de>
2016-09-09 16:38:59 +0200

    Transfer of remaining components of the AuditLogCollector.

--- freamon-monitor/src/main/scala/de/tuberlin/cit/freamon/monitor/utils/AuditLogManager.scala
+++ freamon-monitor/src/main/scala/de/tuberlin/cit/freamon/monitor/utils/AuditLogManager.scala
@@ -17,7 +17,7 @@ object AuditLogManager {
       pathToLog = args(3)
       loadDriver("nl.cwi.monetdb.jdbc.MonetDriver")
       createTable()
-      start(args(3))
+      start(pathToLog)
     }
   }
 
@@ -82,5 +82,37 @@ object AuditLogManager {
       }
     tryExists(1)
   }
+
+}
+
+import java.io.{File, InputStream}
+
+class FollowingInputStream(val file: File, val waitForNewInput: () => Unit) extends InputStream{
+
+  import java.io.FileInputStream
+  private val underlying = new FileInputStream(file)
+
+  def read: Int = handle(underlying.read)
+
+  override def read(b: Array[Byte]): Int = read(b, 0, b.length)
+
+  override def read(b: Array[Byte], off: Int, len: Int): Int = handle(underlying.read(b, off, len))
+
+  override def close(): Unit = underlying.close
+
+  protected def rotated_? = try { underlying.getChannel.position() > file.length }
+
+  protected def closed_? = !underlying.getChannel.isOpen
+
+  protected def handle(read: => Int): Int = read match {
+    case -1 if rotated_? || closed_? => -1
+    case -1 =>
+      waitForNewInput()
+      handle(read)
+    case i => i
+  }
+
+  require(file != null)
+  assume(file.exists)
 
 }
