cf371db053e5d833d87907859d0b35b79f7246fe
Adrian Warszewski <a.warszewski@campus.tu-berlin.de>
2016-09-14 20:36:53 +0200

    Moving a selection of functions into the extra thread

--- freamon-collector/src/main/scala/de/tuberlin/cit/freamon/collector/AuditLogCollector.scala
+++ freamon-collector/src/main/scala/de/tuberlin/cit/freamon/collector/AuditLogCollector.scala
@@ -85,11 +85,66 @@ object AuditLogCollector{
           }
           read
         }
+        def processEntry(entry: String): de.tuberlin.cit.freamon.api.AuditLogEntry = {
+          val auditLog:de.tuberlin.cit.freamon.api.AuditLogEntry = new de.tuberlin.cit.freamon.api.AuditLogEntry
+          val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS")
+
+          val date: String = entry.substring(0,23)
+          auditLog.date = sdf.parse(date).getTime
+          val report = entry.substring(49)
+          val splitReport: Array[String] = report.split("\t")
+          val allowed = splitReport(0).substring(8)
+          if(allowed=="true")
+            auditLog.allowed = true
+          else if (allowed=="false")
+            auditLog.allowed = false
+          auditLog.ugi = splitReport(1).substring(4)
+          auditLog.ip = splitReport(2).substring(3)
+          auditLog.cmd = splitReport(3).substring(4)
+          auditLog.src =splitReport(4).substring(4)
+          auditLog.dst = splitReport(5).substring(4)
+          auditLog.perm = splitReport(6).substring(5)
+          auditLog.proto = splitReport(7).substring(6)
+
+          auditLog
+
+        }
+
+        def follow(file: File): InputStream = {
+          val maxRetries = 3
+          val waitToOpen = 1000
+          val waitBetweenReads = 100
+
+          def sleep(msec: Long) = () => Thread.sleep(msec)
+
+          follow2(file, maxRetries, sleep(waitToOpen), sleep(waitBetweenReads))
+        }
+
+        def follow2(file: File, openTries: Int, openSleep: () => Unit, rereadSleep: () => Unit): InputStream = {
+          import java.io.SequenceInputStream
+
+          val e = new util.Enumeration[InputStream]() {
+            def nextElement = new FollowingInputStream(file, rereadSleep)
+            def hasMoreElements = testExists(file, openTries, openSleep)
+          }
+          new SequenceInputStream(e)
+
+        }
+        def testExists(file: File, tries: Int, sleep: () => Unit): Boolean = {
+          def tryExists(n: Int): Boolean =
+            if(file.exists()) true
+            else if (n > tries) false
+            else {
+              sleep()
+              tryExists(n+1)
+            }
+          tryExists(1)
+        }
       }
     })
     t.start
   }
-  def processFile(path: String): Unit = {
+  /*def processFile(path: String): Unit = {
     def t = new Thread(new Runnable {
       override def run(): Unit = {
         val logFile = new File(path)
@@ -105,7 +160,7 @@ object AuditLogCollector{
       }
     })
     t.start
-  }
+  }*/
 
   private def processEntry(entry: String): de.tuberlin.cit.freamon.api.AuditLogEntry = {
     val auditLog:de.tuberlin.cit.freamon.api.AuditLogEntry = new de.tuberlin.cit.freamon.api.AuditLogEntry
