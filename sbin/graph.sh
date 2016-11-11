d85af4055e8755b9ec3146bf99b880b582471fcc
Julian <julian.g.boehm@campus.tu-berlin.de>
2016-11-11 14:13:10 +0100

    update graph.sh to execution_unit

--- sbin/graph.sh
+++ sbin/graph.sh
@@ -16,26 +16,25 @@ img_path="${app_id}_$event_kind.png"
 rm -rf "$tmp_csv_dir"
 mkdir -p "$tmp_csv_dir"
 
-echo "Finding workers..."
-workers="`mclient -d freamon -f csv -s "
-    select distinct worker.container_id
-    from event, worker, job
+echo "Finding execution units..."
+execUnits="`mclient -d freamon -f csv -s "
+    select distinct execution_unit.id
+    from event, execution_unit, job
     where value > 0
       and app_id = '$app_id'
-      and job.id = worker.job_id
-      and worker.id = event.worker_id;"`"
+      and job.id = execution_unit.job_id
+      and execution_unit.id = event.execution_unit_id;"`"
 
-echo "Collecting data for each worker..."
-for worker in $workers; do
+echo "Collecting data for each execution_unit..."
+for execUnit in $execUnits; do
     query="
         select millis * 0.001, value
-        from event, worker
+        from event
         where kind='$event_kind'
-          and worker.container_id='$worker'
-          and worker.id = event.worker_id
+          and execution_unit_id='$execUnit'
         order by millis asc;"
     # this filename appears in the plot
-    csv_path="$tmp_csv_dir/$worker.csv"
+    csv_path="$tmp_csv_dir/$execUnit.csv"
     mclient -d freamon -f csv -s "$query" > "$csv_path"
 done
 
